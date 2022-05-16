865e3ff438dde04986930beb11a04cb73887a113
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-05-16 12:32:22 +0100

    removed console.log

--- models/categories.js
+++ models/categories.js
@@ -2,7 +2,6 @@ const db = require ("../db/connection")
 
 exports.selectCategories = () =>{
     return db.query("SELECT * FROM categories").then((categories) =>{
-        console.log(categories)
         return categories.rows;
     })
 }
