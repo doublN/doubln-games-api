2808fffcab660e168dde89fce68759ae273b7840
Nathan Newberry <nathannewberry@172-10-254-188.lightspeed.fyvlar.sbcglobal.net>
2022-05-20 12:36:20 +0100

    Fixed GET /api behaviour so the correct response is sent. Test was not functioning properly so fixed it so it is

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -4,17 +4,16 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
 const fs = require("fs/promises");
-const { hasUncaughtExceptionCaptureCallback } = require("process");
 
 describe("GET /api", () =>{
     test("status 200: responds with a json object containing all the endpoints", () =>{
         return request(app)
         .get("/api")
         .expect(200)
-        .then(({body}) =>{
-            Promise.all([fs.readFile("./endpoints.json", "utf-8"), body.endpoints]);
-        }).then((file, endpoints) =>{
-            expect(endpoints).toEqual(file);
+        .then(({text}) =>{
+            return Promise.all([fs.readFile("./endpoints.json", "utf-8"), text]);
+        }).then(([file, text]) =>{
+            expect(text).toEqual(file);
         })
     })
 })
--- controllers/api.js
+++ controllers/api.js
@@ -2,6 +2,6 @@ fs = require("fs/promises")
 
 exports.getEndpoints = ((req, res) =>{
     fs.readFile("./endpoints.json", "utf-8").then((endpoints) =>{
-        res.status(200).send({endpoints})
+        res.status(200).send(endpoints)
     })
 })
